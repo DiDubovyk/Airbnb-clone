6cd567a7e76b24010df291d7b4a3d2499b19fddd
unknown <diana.dubovik33@gmail.com>
2025-02-18 10:56:54 +0100

    Updated listing

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,4 +1,5 @@
 import prisma from "@/app/libs/prismadb"
+import { listenerCount } from "node:stream";
 
 
 export default async function getListings() {
@@ -7,8 +8,13 @@ export default async function getListings() {
             orderBy: {
                 createdAt: 'desc'
             }
-        })
-        return listings;
+        });
+        const safeListings = listings.map((listing) => ({
+            ...listing,
+            createdAt: listing.createdAt.toISOString(),
+        }));
+
+        return safeListings;
     } catch (error: any) {
         throw new Error(error)
     }
